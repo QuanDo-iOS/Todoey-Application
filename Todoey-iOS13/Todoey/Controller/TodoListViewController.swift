7f591288a8a30db677897bf56115a46bbefa2ea9
MinhQuan-Github <minhquan.5322@aol.com>
2022-03-13 23:13:13 +0700

    Save Data with Core Data

--- Todoey-iOS13/Todoey/Controller/TodoListViewController.swift
+++ Todoey-iOS13/Todoey/Controller/TodoListViewController.swift
@@ -7,24 +7,27 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     var itemArray = [Item]()
     // let defaults = UserDefaults.standard
     let dataPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print(dataPath!)
         // load data
-        self.loadData()
+//        self.loadData()
+        
 //        if let item = defaults.array(forKey: "TodoListArray") as? [Item] {
 //            self.itemArray = item
 //        }
     }
     
-    //MARK - TableView DataSource Methods
+    // MARK: - TableView DataSource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.itemArray.count
     }
@@ -38,21 +41,25 @@ class TodoListViewController: UITableViewController {
         return cell;
     }
     
-    //MARK - TableView Delegate Methods
+    // MARK: - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         self.saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    //MARK - Add New Item
+    // MARK: - Add New Item
     @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
-            self.itemArray.append(Item(title: textField.text!, done: false))
-            // set data on device
+            // set data for item
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            // append to itemArray
+            self.itemArray.append(newItem)
             //self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.saveItems()
         }
@@ -64,25 +71,29 @@ class TodoListViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    // MARK: - save item data
     func saveItems() {
-        let encoder = PropertyListEncoder()
+//        let encoder = PropertyListEncoder()
         do {
-            let data = try encoder.encode(self.itemArray)
-            try data.write(to: self.dataPath!)
+//            let data = try encoder.encode(self.itemArray)
+//            try data.write(to: self.dataPath!)
+            try self.context.save()
         } catch {
-            print("Error encoding item array \(error)")
+//            print("Error encoding item array \(error)")
+            print("error saving context \(error)")
         }
         tableView.reloadData()
     }
-
-    func loadData() {
-        if let data = try? Data(contentsOf: self.dataPath!){
-            let decoder = PropertyListDecoder()
-            do {
-                self.itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error decode item array \(error)")
-            }
-        }
-    }
+    
+    // MARK: - load item data
+//    func loadData() {
+//        if let data = try? Data(contentsOf: self.dataPath!){
+//            let decoder = PropertyListDecoder()
+//            do {
+//                self.itemArray = try decoder.decode([Item].self, from: data)
+//            } catch {
+//                print("Error decode item array \(error)")
+//            }
+//        }
+//    }
 } 
